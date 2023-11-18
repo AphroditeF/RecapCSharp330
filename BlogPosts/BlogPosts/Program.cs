f5332667a30e046a896b8b41a6a2d2339a3e5e91
AphroditeF <a.flora@outlook.com>
2023-11-18 12:14:37 +0200

    Post and Image Classes

--- BlogPosts/BlogPosts/Program.cs
+++ BlogPosts/BlogPosts/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BlogPosts;
+using System;
 
 namespace MyApp // Note: actual namespace depends on the project name.
 {
@@ -6,7 +7,17 @@ namespace MyApp // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Post post1 = new Post("Thanks for the birthday wishes", true, "Denis Panjuta");
+            Console.WriteLine(post1.ToString());
+
+            ImagePost imagePost1 = new ImagePost("Check out my new shoes", "Denis Panjuta", "https://images.com/shoes", true);
+
+
+            Console.WriteLine(imagePost1.ToString());
+            
+            Console.ReadLine();
         }
+
+
     }
 }
